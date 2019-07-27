0939196cd9063f7de07dc076cae618a5852ae307
Jakobson <jakobson.carlos@mgail.com>
2019-07-27 09:21:24 -0300

    Uri_1008
    Add Question 1008

---
+++ Uri_1008.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Uri_1008 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a,b;
+		double c;
+		
+	Scanner sc = new Scanner(System.in);
+		
+		
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = sc.nextFloat();
+		
+		double SALARY = (b*c);
+		
+		System.out.println("NUMBER = "+a);
+		System.out.println("SALARY = U$ %0.2f "+SALARY);
+
+	}
+
+}
