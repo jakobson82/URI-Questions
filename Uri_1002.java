6617b34b20eb930775b52c57de3ed11b1058a159
Jakobson <jakobson.carlos@mgail.com>
2019-07-27 08:11:02 -0300

    Create Uri_1002.java
    Add URI-1002

---
+++ Uri_1002.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Uri_1002 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		Scanner sc = new Scanner(System.in);
+		
+		double r = sc.nextDouble();
+		
+		double pi = 3.14159;
+		double a; 
+		a= pi * Math.pow(r, 2);
+		
+		System.out.println("A=%.4f"+a);
+        System.out.println();
+		
+		
+	}
+
+}
