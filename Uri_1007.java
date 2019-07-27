2102a2f51d02830a4a5f928c5a65221ca71047e7
Jakobson <jakobson.carlos@mgail.com>
2019-07-27 08:25:35 -0300

    Uri_1007.java
    Add question 1007

---
+++ Uri_1007.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Uri_1007 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int A,B,C,D,DIFERENCA;
+		
+		A = sc.nextInt();
+		B = sc.nextInt();
+		C = sc.nextInt();
+		D = sc.nextInt();
+		DIFERENCA = (A * B - C * D);
+		
+		System.out.println("DIFERENCA = "+DIFERENCA);
+        System.out.println();
+
+	}
+
+}
